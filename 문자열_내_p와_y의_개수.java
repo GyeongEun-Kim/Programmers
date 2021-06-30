18afd5942f10f03af9e17d39fc0f54e6640ed874
GyeongEun Kim <kekim20@naver.com>
2021-06-30 17:20:20 +0900

    21/06/30

---
+++ 문자열_내_p와_y의_개수.java
@@ -0,0 +1,22 @@
+public class 문자열_내_p와_y의_개수 {
+    class Solution {
+        boolean solution(String s) {
+            boolean answer = true;
+            int p = 0;
+            int y =0;
+
+            for (int i=0;i<s.length();i++) {
+                if (s.charAt(i)=='p'|| s.charAt(i)=='P')
+                    p++;
+                else if (s.charAt(i)=='y' || s.charAt(i)=='Y')
+                    y++;
+            }
+
+            if (p!=y)
+                answer=false;
+
+
+            return answer;
+        }
+    }
+}
