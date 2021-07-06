13424eb123e8426e55e42217a650260726482216
GyeongEun Kim <kekim20@naver.com>
2021-07-06 18:24:34 +0900

    21/07/06

---
+++ 서울에서김서방찾기.java
@@ -0,0 +1,15 @@
+public class 서울에서김서방찾기 {
+    class Solution {
+        public String solution(String[] seoul) {
+
+            int idx=0;
+            for (int i=0;i<seoul.length;i++) {
+
+                if (seoul[i].equals("Kim"))
+                    idx=i;
+            }
+
+            return "김서방은 "+idx+"에 있다";
+        }
+    }
+}
