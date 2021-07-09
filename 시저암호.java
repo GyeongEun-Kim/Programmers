5818be38d464911c3928ea8076434ec60625000f
GyeongEun Kim <kekim20@naver.com>
2021-07-09 21:53:00 +0900

    21/07/09

---
+++ 시저암호.java
@@ -0,0 +1,33 @@
+public class 시저암호 {
+    class Solution {
+        public String solution(String s, int n) {
+            int temp;
+            StringBuilder sb = new StringBuilder();
+
+            for (int i=0;i<s.length();i++) {
+                if (s.charAt(i)==' ') {
+                    sb.append(s.charAt(i));
+                }
+                //A=65, a=97, Z=90, z=122
+                else if (s.charAt(i)>='A' && s.charAt(i)<='Z') { //대문자
+                    temp=((int)s.charAt(i))+n;
+                    if (temp>90)
+                        sb.append((char)(temp-90+65-1));
+                    else
+                        sb.append((char)temp);
+                }
+                else {//소문자
+                    temp=((int)s.charAt(i))+n;
+
+                    if (temp>122)
+                        sb.append((char)(temp-122+97-1));
+                    else
+                        sb.append((char)temp);
+                }
+            }
+
+            return sb.toString();
+        }
+
+    }
+}
